2d94b4cf47fdf38feae628d1d4cac8fe94a173c3
Gavin Crawford <94875769+gavincrawford@users.noreply.github.com>
2025-01-07 21:48:46 -0700

    Refactor lexer tests into another file

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,5 +1,7 @@
 //! The lexer breaks down text information into tokens, which can be used to assemble syntax.
 
+mod tests;
+
 /// Represents all possible tokens.
 #[derive(Debug, PartialEq)]
 pub enum Token {
@@ -92,77 +94,5 @@ impl Lexer {
         }
         tokens.push(Token::Endl);
         tokens
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use Token::*;
-
-    #[test]
-    fn variable_assignment() {
-        assert_eq!(
-            Lexer::new().lex("let x".into()),
-            vec![Let, Identifier("x".into()), Endl]
-        );
-    }
-
-    #[test]
-    fn math() {
-        assert_eq!(
-            Lexer::new().lex("1 + 1".into()),
-            vec![Number(1.), Add, Number(1.), Endl]
-        );
-        assert_eq!(
-            Lexer::new().lex("1 - 1".into()),
-            vec![Number(1.), Sub, Number(1.), Endl]
-        );
-        assert_eq!(
-            Lexer::new().lex("1 * 1".into()),
-            vec![Number(1.), Mul, Number(1.), Endl]
-        );
-        assert_eq!(
-            Lexer::new().lex("1 / 1".into()),
-            vec![Number(1.), Div, Number(1.), Endl]
-        );
-    }
-
-    #[test]
-    fn logic() {
-        assert_eq!(
-            Lexer::new().lex("1 == 2".into()),
-            vec![Number(1.), LogicalEq, Number(2.), Endl]
-        );
-        assert_eq!(
-            Lexer::new().lex("1 != 2".into()),
-            vec![Number(1.), LogicalNeq, Number(2.), Endl]
-        );
-        assert_eq!(
-            Lexer::new().lex("1 > 2".into()),
-            vec![Number(1.), LogicalG, Number(2.), Endl]
-        );
-        assert_eq!(
-            Lexer::new().lex("1 < 2".into()),
-            vec![Number(1.), LogicalL, Number(2.), Endl]
-        );
-    }
-
-    #[test]
-    fn conditionals() {
-        assert_eq!(
-            // TODO fix. this test SHOULD pass, but the lexer does not recognize the brackets as
-            // separate characters, and instead converts them into an indentifier
-            Lexer::new().lex("if 1 > 2 {}".into()),
-            vec![
-                If,
-                Number(1.),
-                LogicalG,
-                Number(2.),
-                BlockStart,
-                BlockEnd,
-                Endl
-            ]
-        );
     }
 }
