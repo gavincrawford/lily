4d7b57b89990e3f1dd804a5cb6e111cb77f52d7e
Gavin Crawford <94875769+gavincrawford@users.noreply.github.com>
2025-01-07 21:32:50 -0700

    Math operations
    Also added tests. Language now supports semicolon-less endlines.

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,12 +1,22 @@
 //! The lexer breaks down text information into tokens, which can be used to assemble syntax.
 
 /// Represents all possible tokens.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Token {
+    // variables
     Equal,
-    Identifier,
+    Identifier(String),
     Let,
+
+    // math
     Number(f32),
+    Add,
+    Sub,
+    Mul,
+    Div,
+
+    // other
+    Endl,
 }
 
 pub struct Lexer;
@@ -18,16 +28,21 @@ impl Lexer {
 
     /// Lexes the provided file, as a string, into a vector of tokens.
     pub fn lex(&mut self, buf: String) -> Vec<Token> {
-        // convert buf to lines by splitting on semicolons
-        let buf = buf.split(";");
+        // remove unneeded escapes and split by ';'
+        let buf = buf.replace(";", "\n");
+        let buf = buf.split("\n");
 
-        // iterate over lines
+        // generate tokens
         let mut tokens = vec![];
         for line in buf {
+            // skip blank lines
+            if line == "" {
+                continue;
+            }
+
+            // parse line and store tokens
             tokens.extend(self.lex_ln(line.split_whitespace()));
         }
-
-        // return tokens
         tokens
     }
 
@@ -41,14 +56,53 @@ impl Lexer {
                 "let" => Let,
                 "=" => Equal,
 
-                // numeric
+                // math
                 s if s.parse::<f32>().is_ok() => Number(s.parse::<f32>().unwrap()),
+                "+" => Add,
+                "-" => Sub,
+                "*" => Mul,
+                "/" => Div,
 
                 // other
                 "--" => break,
-                _ => Identifier,
+                _ => Identifier(word.into()),
             });
         }
+        tokens.push(Token::Endl);
         tokens
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use Token::*;
+
+    #[test]
+    fn variable_assignment() {
+        assert_eq!(
+            Lexer::new().lex("let x".into()),
+            vec![Let, Identifier("x".into()), Endl]
+        );
+    }
+
+    #[test]
+    fn math() {
+        assert_eq!(
+            Lexer::new().lex("1 + 1".into()),
+            vec![Number(1.), Add, Number(1.), Endl]
+        );
+        assert_eq!(
+            Lexer::new().lex("1 - 1".into()),
+            vec![Number(1.), Sub, Number(1.), Endl]
+        );
+        assert_eq!(
+            Lexer::new().lex("1 * 1".into()),
+            vec![Number(1.), Mul, Number(1.), Endl]
+        );
+        assert_eq!(
+            Lexer::new().lex("1 / 1".into()),
+            vec![Number(1.), Div, Number(1.), Endl]
+        );
     }
 }
