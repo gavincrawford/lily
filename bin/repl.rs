a9d15c633bb96eda970fa7c4ab4b8d0ba6286d37
Gavin Crawford <94875769+gavincrawford@users.noreply.github.com>
2025-01-11 00:01:31 -0700

    Add REPL for testing

---
+++ bin/repl.rs
@@ -0,0 +1,17 @@
+use lily::{interpreter::*, lexer::*, parser::*};
+use std::io::{stdin, BufRead};
+
+fn main() {
+    let mut interpreter = Interpreter::new();
+    loop {
+        interpret_line(&mut interpreter);
+        dbg!(&interpreter.global_scope);
+    }
+}
+
+fn interpret_line(interpreter: &mut Interpreter) {
+    let mut buf = String::new();
+    stdin().lock().read_line(&mut buf).unwrap();
+    let ast = Parser::new(Lexer::new().lex(buf)).parse();
+    interpreter.execute(ast);
+}
