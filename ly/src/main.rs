6fea2f1b445c3559d7faab72137c1296317bbb49
Gavin Crawford <94875769+gavincrawford@users.noreply.github.com>
2025-03-04 14:45:02 -0700

    (ly) Properly handle errors

--- ly/src/main.rs
+++ ly/src/main.rs
@@ -1,66 +1,57 @@
-use lylib::{interpreter::*, lexer::*, parser::*};
-use std::{env, fs, path::PathBuf, process, rc::Rc};
+use lylib::{
+    anyhow::{Context, Result},
+    interpreter::*,
+    lexer::*,
+    parser::*,
+};
+use std::{env, fs, path::PathBuf, rc::Rc};
 
 fn main() {
-    // read file to buffer
     let args: Vec<String> = env::args().collect();
     let file_path = &args.get(1).expect("no file provided.");
-    let buf = match fs::read_to_string(file_path) {
-        Ok(contents) => contents,
-        Err(err) => {
-            eprintln!("Error reading file {}: {}", file_path, err);
-            process::exit(1);
+    match run_file(file_path) {
+        Err(e) => {
+            eprintln!("{:?}", e);
         }
-    };
+        _ => {}
+    }
+}
+
+/// Runs a file.
+fn run_file(file_path: &&String) -> Result<()> {
+    //read file to buffer
+    let buf = fs::read_to_string(file_path).context("failed to open file")?;
 
     // get pwd
     let mut path = PathBuf::from(file_path);
     path.pop();
 
     // lex buffer into tokens
-    let tokens = match Lexer::new().lex(buf) {
-        Ok(toks) => toks,
-        Err(e) => {
-            eprintln!("LEX ERR: {:?}\n", e);
-            panic!();
-        }
-    };
+    let tokens = Lexer::new().lex(buf).context("failed to lex file")?;
 
     // parse tokens into ast
     let mut parser = Parser::new(tokens);
     parser.set_pwd(path);
-    let ast = match parser.parse_with_imports(stdlib()) {
-        Ok(tree) => tree,
-        Err(e) => {
-            eprintln!("PARSE ERR: {:?}\n", e);
-            panic!();
-        }
-    };
+    let ast = parser
+        .parse_with_imports(stdlib().context("failed to resolve standard library")?)
+        .context("failed to parse file")?;
 
     // execute interpreter
     let mut interp = Interpreter::new();
-    match interp.execute(ast) {
-        Ok(_) => {}
-        Err(e) => {
-            eprintln!("EXEC ERR: {:?}\n", e);
-            panic!();
-        }
-    }
+    interp.execute(ast).context("failed to execute file")?;
 
     // for debugging
     #[cfg(debug_assertions)]
     dbg!(interp.memory.borrow().inner());
+    Ok(())
 }
 
 /// Creates STD module import.
-fn stdlib() -> Vec<Rc<ASTNode>> {
-    // TODO more informative error messages here, no unwraps
+fn stdlib() -> Result<Vec<Rc<ASTNode>>> {
     let mut lexer = Lexer::new();
-    vec![ASTNode::Module {
+    Ok(vec![ASTNode::Module {
         alias: Some("math".into()),
-        body: Parser::new(lexer.lex(include_str!("./std/math.ly").into()).unwrap())
-            .parse()
-            .unwrap(),
+        body: Parser::new(lexer.lex(include_str!("./std/math.ly").into()).unwrap()).parse()?,
     }
-    .into()]
+    .into()])
 }
