71aba4524b6f357198c6c49a626f6c05eba594cd
Gavin Crawford <94875769+gavincrawford@users.noreply.github.com>
2025-07-21 09:36:05 -0600

    (lylib) Execution benchmarks

---
+++ lylib/benches/interpret.rs
@@ -0,0 +1,45 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use lylib::{
+    interpreter::Interpreter,
+    lexer::Lexer,
+    parser::{ASTNode, Parser},
+};
+use std::{
+    hint::black_box,
+    io::{stdin, stdout},
+    rc::Rc,
+};
+
+macro_rules! ast {
+    ($src:expr) => {
+        black_box(
+            Parser::new(Lexer::new().lex(include_str!($src).to_string()).unwrap())
+                .parse()
+                .unwrap(),
+        )
+    };
+}
+
+fn interpret(ast: Rc<ASTNode>) {
+    let mut i = Interpreter::new(stdin(), stdout());
+    i.execute(ast).expect("failed to execute benchmark AST.");
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("fibonacci", |b| {
+        b.iter(|| interpret(ast!("../src/interpreter/tests/implementation/fibonacci.ly")))
+    });
+    c.bench_function("matrix rotation", |b| {
+        b.iter(|| {
+            interpret(ast!(
+                "../src/interpreter/tests/implementation/matrix_rotation.ly"
+            ))
+        })
+    });
+    c.bench_function("tree", |b| {
+        b.iter(|| interpret(ast!("../src/interpreter/tests/implementation/tree.ly")))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
