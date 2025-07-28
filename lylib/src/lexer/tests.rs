1859417dc61b74af21a4ffebab107d887bfa62cb
Gavin Crawford <94875769+gavincrawford@users.noreply.github.com>
2025-07-28 01:26:13 -0600

    (lylib:test) Add macros to clean up lexer tests

--- lylib/src/lexer/tests.rs
+++ lylib/src/lexer/tests.rs
@@ -7,353 +7,284 @@
 use super::*;
 use Token::*;
 
-// TODO write new macros to improve on lexer test clarity
+/// Shorthand for lexing input and comparing against expected tokens.
+///
+/// # Examples
+///
+/// ```rust
+/// # use lylib::*;
+/// # use lylib::lexer::Token::*;
+/// lex_eq!("let x = 5;" => Let, Identifier("x".into()), Equal, Number(5.), Endl);
+/// ```
+#[macro_export]
+macro_rules! lex_eq {
+    ($input:expr => $($token:expr),*) => {
+        let result = Lexer::new().lex($input.into());
+        assert!(result.is_ok(), "Lexer failed: {:?}", result);
+        assert_eq!(
+            result.unwrap(),
+            vec![$($token),*]
+        );
+    };
+}
 
 #[test]
 fn variable_assignment() {
-    let result = Lexer::new().lex("let var1 = 1; let var2 = 2;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            Let,
-            Identifier("var1".into()),
-            Equal,
-            Number(1.),
-            Endl,
-            Let,
-            Identifier("var2".into()),
-            Equal,
-            Number(2.),
-            Endl
-        ]
+    lex_eq!("let var1 = 1; let var2 = 2;" =>
+        Let,
+        Identifier("var1".into()),
+        Equal,
+        Number(1.),
+        Endl,
+        Let,
+        Identifier("var2".into()),
+        Equal,
+        Number(2.),
+        Endl
     );
 }
 
 #[test]
 fn math() {
-    let result = Lexer::new().lex("1+1+1+1;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            Number(1.),
-            Add,
-            Number(1.),
-            Add,
-            Number(1.),
-            Add,
-            Number(1.),
-            Endl
-        ]
+    lex_eq!("1+1+1+1;" =>
+        Number(1.),
+        Add,
+        Number(1.),
+        Add,
+        Number(1.),
+        Add,
+        Number(1.),
+        Endl
     );
 
-    let result = Lexer::new().lex("32+12-7;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![Number(32.), Add, Number(12.), Sub, Number(7.), Endl]
+    lex_eq!("32+12-7;" =>
+        Number(32.), Add, Number(12.), Sub, Number(7.), Endl
     );
 
-    let result = Lexer::new().lex("0.5731 * 0.222 / 1^3 // 10;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            Number(0.5731),
-            Mul,
-            Number(0.222),
-            Div,
-            Number(1.),
-            Pow,
-            Number(3.),
-            Floor,
-            Number(10.),
-            Endl
-        ]
+    lex_eq!("0.5731 * 0.222 / 1^3 // 10;" =>
+        Number(0.5731),
+        Mul,
+        Number(0.222),
+        Div,
+        Number(1.),
+        Pow,
+        Number(3.),
+        Floor,
+        Number(10.),
+        Endl
     );
 }
 
 #[test]
 fn logic() {
-    let result = Lexer::new().lex("1 == 2; 1 != 2;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            Number(1.),
-            LogicalEq,
-            Number(2.),
-            Endl,
-            Number(1.),
-            LogicalNeq,
-            Number(2.),
-            Endl
-        ]
+    lex_eq!("1 == 2; 1 != 2;" =>
+        Number(1.),
+        LogicalEq,
+        Number(2.),
+        Endl,
+        Number(1.),
+        LogicalNeq,
+        Number(2.),
+        Endl
     );
 
-    let result = Lexer::new().lex("1 > 2 >= 3; 1 < 2 <= 3;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            Number(1.),
-            LogicalG,
-            Number(2.),
-            LogicalGe,
-            Number(3.),
-            Endl,
-            Number(1.),
-            LogicalL,
-            Number(2.),
-            LogicalLe,
-            Number(3.),
-            Endl
-        ]
+    lex_eq!("1 > 2 >= 3; 1 < 2 <= 3;" =>
+        Number(1.),
+        LogicalG,
+        Number(2.),
+        LogicalGe,
+        Number(3.),
+        Endl,
+        Number(1.),
+        LogicalL,
+        Number(2.),
+        LogicalLe,
+        Number(3.),
+        Endl
     );
 
-    let result = Lexer::new().lex(" true && false; true || false;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            Bool(true),
-            LogicalAnd,
-            Bool(false),
-            Endl,
-            Bool(true),
-            LogicalOr,
-            Bool(false),
-            Endl,
-        ]
+    lex_eq!("true && false; true || false;" =>
+        Bool(true),
+        LogicalAnd,
+        Bool(false),
+        Endl,
+        Bool(true),
+        LogicalOr,
+        Bool(false),
+        Endl
     );
 }
 
 #[test]
 fn conditionals() {
-    let result = Lexer::new().lex("if 1 > 2 do end;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            If,
-            Number(1.),
-            LogicalG,
-            Number(2.),
-            BlockStart,
-            BlockEnd,
-            Endl
-        ]
+    lex_eq!("if 1 > 2 do end;" =>
+        If,
+        Number(1.),
+        LogicalG,
+        Number(2.),
+        BlockStart,
+        BlockEnd,
+        Endl
     );
 
-    let result = Lexer::new().lex("if 1 < 2 do; 1 + 1; end;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            If,
-            Number(1.),
-            LogicalL,
-            Number(2.),
-            BlockStart,
-            Endl,
-            Number(1.),
-            Add,
-            Number(1.),
-            Endl,
-            BlockEnd,
-            Endl
-        ]
+    lex_eq!("if 1 < 2 do; 1 + 1; end;" =>
+        If,
+        Number(1.),
+        LogicalL,
+        Number(2.),
+        BlockStart,
+        Endl,
+        Number(1.),
+        Add,
+        Number(1.),
+        Endl,
+        BlockEnd,
+        Endl
     );
 
-    let result = Lexer::new().lex("if !true do end;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![If, LogicalNot, Bool(true), BlockStart, BlockEnd, Endl]
+    lex_eq!("if !true do end;" =>
+        If, LogicalNot, Bool(true), BlockStart, BlockEnd, Endl
     );
 }
 
 #[test]
 fn functions() {
-    let result =
-        Lexer::new().lex("func func_name a b do; return a + b; end; func_name(a, b)".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            Function,
-            Identifier("func_name".into()),
-            Identifier("a".into()),
-            Identifier("b".into()),
-            BlockStart,
-            Endl,
-            Return,
-            Identifier("a".into()),
-            Add,
-            Identifier("b".into()),
-            Endl,
-            BlockEnd,
-            Endl,
-            Identifier("func_name".into()),
-            ParenOpen,
-            Identifier("a".into()),
-            Comma,
-            Identifier("b".into()),
-            ParenClose,
-        ]
+    lex_eq!("func func_name a b do; return a + b; end; func_name(a, b)" =>
+        Function,
+        Identifier("func_name".into()),
+        Identifier("a".into()),
+        Identifier("b".into()),
+        BlockStart,
+        Endl,
+        Return,
+        Identifier("a".into()),
+        Add,
+        Identifier("b".into()),
+        Endl,
+        BlockEnd,
+        Endl,
+        Identifier("func_name".into()),
+        ParenOpen,
+        Identifier("a".into()),
+        Comma,
+        Identifier("b".into()),
+        ParenClose
     );
 
-    let result = Lexer::new().lex("function(1 + 2, 3 + 4)".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            Identifier("function".into()),
-            ParenOpen,
-            Number(1.),
-            Add,
-            Number(2.),
-            Comma,
-            Number(3.),
-            Add,
-            Number(4.),
-            ParenClose,
-        ]
+    lex_eq!("function(1 + 2, 3 + 4)" =>
+        Identifier("function".into()),
+        ParenOpen,
+        Number(1.),
+        Add,
+        Number(2.),
+        Comma,
+        Number(3.),
+        Add,
+        Number(4.),
+        ParenClose
     );
 }
 
 #[test]
 fn strings() {
-    let result = Lexer::new().lex("\"this is a string\";".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![Str(String::from("this is a string")), Endl,]
+    lex_eq!("\"this is a string\";" =>
+        Str("this is a string".into()), Endl
     );
 }
 
 #[test]
 fn chars() {
-    let result = Lexer::new().lex("'a' 'b' 'c';".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![Char('a'), Char('b'), Char('c'), Endl,]
+    lex_eq!("'a' 'b' 'c';" =>
+        Char('a'), Char('b'), Char('c'), Endl
     );
 }
 
 #[test]
 fn bools() {
-    let result = Lexer::new().lex("true false;".into());
-    assert!(result.is_ok());
-    assert_eq!(result.unwrap(), vec![Bool(true), Bool(false), Endl,]);
+    lex_eq!("true false;" =>
+        Bool(true), Bool(false), Endl
+    );
 }
 
 #[test]
 fn parens() {
-    let result = Lexer::new().lex("(1 + 1) + 1;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            ParenOpen,
-            Number(1.),
-            Add,
-            Number(1.),
-            ParenClose,
-            Add,
-            Number(1.),
-            Endl,
-        ]
+    lex_eq!("(1 + 1) + 1;" =>
+        ParenOpen,
+        Number(1.),
+        Add,
+        Number(1.),
+        ParenClose,
+        Add,
+        Number(1.),
+        Endl
     );
 }
 
 #[test]
 fn lists() {
-    let result = Lexer::new().lex("let list = [0, false, 'a']; let value = list[0];".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            Let,
-            Identifier("list".into()),
-            Equal,
-            BracketOpen,
-            Number(0.),
-            Comma,
-            Bool(false),
-            Comma,
-            Char('a'),
-            BracketClose,
-            Endl,
-            Let,
-            Identifier("value".into()),
-            Equal,
-            Identifier("list".into()),
-            BracketOpen,
-            Number(0.),
-            BracketClose,
-            Endl,
-        ]
+    lex_eq!("let list = [0, false, 'a']; let value = list[0];" =>
+        Let,
+        Identifier("list".into()),
+        Equal,
+        BracketOpen,
+        Number(0.),
+        Comma,
+        Bool(false),
+        Comma,
+        Char('a'),
+        BracketClose,
+        Endl,
+        Let,
+        Identifier("value".into()),
+        Equal,
+        Identifier("list".into()),
+        BracketOpen,
+        Number(0.),
+        BracketClose,
+        Endl
     );
 }
 
 #[test]
 fn loops() {
-    let result = Lexer::new().lex("while true do; end;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![While, Bool(true), BlockStart, Endl, BlockEnd, Endl]
+    lex_eq!("while true do; end;" =>
+        While, Bool(true), BlockStart, Endl, BlockEnd, Endl
     );
 }
 
 #[test]
 fn modules() {
-    let result =
-        Lexer::new().lex("import \"./module.ly\"; import \"./module.ly\" as alias;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            Import,
-            Str("./module.ly".into()),
-            Endl,
-            Import,
-            Str("./module.ly".into()),
-            As,
-            Identifier("alias".into()),
-            Endl
-        ]
+    lex_eq!("import \"./module.ly\"; import \"./module.ly\" as alias;" =>
+        Import,
+        Str("./module.ly".into()),
+        Endl,
+        Import,
+        Str("./module.ly".into()),
+        As,
+        Identifier("alias".into()),
+        Endl
     );
 }
 
 #[test]
 fn structs() {
-    let result =
-        Lexer::new().lex("struct Number do; let value = 0; end; let instance = new Number;".into());
-    assert!(result.is_ok());
-    assert_eq!(
-        result.unwrap(),
-        vec![
-            Struct,
-            Identifier("Number".into()),
-            BlockStart,
-            Endl,
-            Let,
-            Identifier("value".into()),
-            Equal,
-            Number(0.),
-            Endl,
-            BlockEnd,
-            Endl,
-            Let,
-            Identifier("instance".into()),
-            Equal,
-            New,
-            Identifier("Number".into()),
-            Endl,
-        ]
+    lex_eq!("struct Number do; let value = 0; end; let instance = new Number;" =>
+        Struct,
+        Identifier("Number".into()),
+        BlockStart,
+        Endl,
+        Let,
+        Identifier("value".into()),
+        Equal,
+        Number(0.),
+        Endl,
+        BlockEnd,
+        Endl,
+        Let,
+        Identifier("instance".into()),
+        Equal,
+        New,
+        Identifier("Number".into()),
+        Endl
     );
 }
