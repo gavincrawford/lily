59bc074984d07b6226ea318e9507f19b2b697cb5
Gavin Crawford <94875769+gavincrawford@users.noreply.github.com>
2025-08-01 11:07:42 -0600

    refactor: Use new `execute` helper in CLI

--- ly/src/execute.rs
+++ ly/src/execute.rs
@@ -1,15 +1,8 @@
 use clap::ArgMatches;
-use lylib::{
-    anyhow::{Context, Result},
-    get_global_interner, intern,
-    interpreter::*,
-    lexer::*,
-    parser::*,
-};
+use lylib::anyhow::{Context, Result};
 use std::{
     fs,
     io::{stdin, stdout},
-    rc::Rc,
 };
 
 /// Executes a file.
@@ -18,37 +11,19 @@ pub fn execute(args: ArgMatches) -> Result<()> {
     let file_path: &String = args.get_one("file").unwrap();
     let buf = fs::read_to_string(file_path).context("failed to open file")?;
 
-    // lex buffer into tokens
-    let tokens = Lexer::new().lex(buf).context("failed to lex file")?;
-
-    // parse tokens into ast
-    let mut parser = Parser::new(tokens);
-    let ast = parser
-        .parse_with_imports(match args.get_one("nostd").unwrap() {
-            true => vec![],
-            false => stdlib().context("failed to resolve standard library")?,
-        })
-        .context("failed to parse file")?;
-    if *args.get_one("debugast").unwrap() {
-        println!("[BUILT AST]\n{:#?}\n", ast);
-    }
-
-    // execute interpreter
-    let mut interp = Interpreter::new(stdin(), stdout());
-    interp.execute(ast).context("failed to execute file")?;
+    // execute file
+    let interp = lylib::execute(
+        buf,
+        stdout(),
+        stdin(),
+        // NOTE: All imports of this style are added to base scope, and don't require importing to
+        // be used. I want to figure out a solution where imports are only used when needed, and
+        // aren't automatically globbed into memory when they aren't needed.
+        vec![include_str!("./std/math.ly").to_string()],
+    )?;
 
     // for debugging
     #[cfg(debug_assertions)]
     println!("{}", interp.memory.borrow());
     Ok(())
-}
-
-/// Creates STD module import.
-fn stdlib() -> Result<Vec<Rc<ASTNode>>> {
-    let mut lexer = Lexer::new();
-    Ok(vec![ASTNode::Module {
-        alias: Some(intern!("math")),
-        body: Parser::new(lexer.lex(include_str!("./std/math.ly").into()).unwrap()).parse()?,
-    }
-    .into()])
 }
