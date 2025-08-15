1836c83d1ce849b9f1993081d5ba08737f41fbc9
Gavin Crawford <94875769+gavincrawford@users.noreply.github.com>
2025-08-15 14:57:18 -0600

    (lylib:style) Remove redundant assertion in `execute_function`
    This can be replaced with an unchecked index because we already assert that the length of the call and expected arguments are equal.

--- lylib/src/interpreter/execute_function.rs
+++ lylib/src/interpreter/execute_function.rs
@@ -17,10 +17,9 @@ impl<Out: Write, In: Read> Interpreter<Out, In> {
             assert_eq!(call_args.len(), fn_args.len());
             self.scope_id += 1;
             for (idx, arg_sym) in fn_args.iter().enumerate() {
-                let arg_expr = call_args.get(idx).unwrap(); // safety: assertion
                 self.declare(
                     &arg_sym.as_id(),
-                    Variable::Owned(ASTNode::inner_to_owned(arg_expr)),
+                    Variable::Owned(ASTNode::inner_to_owned(&call_args[idx])),
                 )?;
             }
 
