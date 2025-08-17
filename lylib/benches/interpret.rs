4e302be403edaf8aebb4a412a1709dcb62a363e7
Gavin Crawford <94875769+gavincrawford@users.noreply.github.com>
2025-08-16 22:43:31 -0600

    (lylib:chore) Improve benchmark stability with in-memory in/out handles & `LargeInput` batch qualifier

--- lylib/benches/interpret.rs
+++ lylib/benches/interpret.rs
@@ -6,7 +6,7 @@ use lylib::{
 };
 use std::{
     hint::black_box,
-    io::{stdin, stdout},
+    io::{self, Cursor},
     rc::Rc,
 };
 
@@ -20,31 +20,32 @@ macro_rules! ast {
     };
 }
 
-fn interpret(ast: Rc<ASTNode>) {
-    let mut i = Interpreter::new(stdin(), stdout());
-    i.execute(ast).expect("failed to execute benchmark AST.");
-}
-
 fn criterion_benchmark(c: &mut Criterion) {
+    fn interpret(ast: Rc<ASTNode>) {
+        // Direct in/out to in-memory locations to avoid introducing system instability
+        let mut i = Interpreter::new(Cursor::new(""), io::sink());
+        i.execute(ast).expect("failed to execute benchmark AST.");
+    }
+
     c.bench_function("fibonacci", |b| {
         b.iter_batched(
             || ast!("../src/interpreter/tests/implementation/fibonacci.ly"),
             |ast| interpret(ast),
-            BatchSize::SmallInput,
+            BatchSize::LargeInput,
         )
     });
     c.bench_function("matrix rotation", |b| {
         b.iter_batched(
             || ast!("../src/interpreter/tests/implementation/matrix_rotation.ly"),
             |ast| interpret(ast),
-            BatchSize::SmallInput,
+            BatchSize::LargeInput,
         )
     });
     c.bench_function("tree", |b| {
         b.iter_batched(
             || ast!("../src/interpreter/tests/implementation/tree.ly"),
             |ast| interpret(ast),
-            BatchSize::SmallInput,
+            BatchSize::LargeInput,
         )
     });
 }
